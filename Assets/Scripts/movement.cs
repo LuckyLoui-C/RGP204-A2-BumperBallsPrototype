6db24279b3162a89bfaa59b235db0cf0474c717f
goodweathergames <spring.leah11@gmail.com>
2022-03-09 12:23:41 +1100

    Fix player movement to move.

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class movement : MonoBehaviour
-{
+public class movement : MonoBehaviour {
     private Rigidbody rb;
 
     public float speed = 0;
     private float movementX;
+
     private float movementY;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         rb = gameObject.GetComponent<Rigidbody>();
     }
 
-    private void OnMove(InputValue movementValue)
+    public void Move(InputAction.CallbackContext context) {
+        Vector2 movementVector = context.ReadValue<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    public void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
 
@@ -28,6 +35,7 @@ public class movement : MonoBehaviour
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
-        rb.AddForce(movement * speed);
+        rb.AddForce(movement * speed, ForceMode.Impulse);
+        // transform.position += movement;
     }
 }

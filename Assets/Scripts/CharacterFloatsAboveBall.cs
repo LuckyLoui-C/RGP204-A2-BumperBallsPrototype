5fbd43f0a82a00acfeb3033b05363e89eb19bb63
Joseph <joe.kallman@gmail.com>
2022-03-15 11:11:57 +1100

    The Characters float and face in the right direction
    Added one script to the project
    Add the script to the character, do not make it a child object of the ball.
    Drag and drop the ball into the public transform on the script
    The character will float above this ball and look in the direction it is facing
    Added a public distance above ball variable to change the height it floats above depending on the specific character so it looks good.

---
+++ Assets/Scripts/CharacterFloatsAboveBall.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterFloatsAboveBall : MonoBehaviour
+{
+    public GameObject playerTransform; // The balls transform
+    public float characterDistanceAboveBall = 0.9f; // How far above the ball the player floats
+    Vector3 previousPosition;
+    Vector3 direction;
+    void Update()
+    {    // The balls transform + how far above you want the character to appear
+        transform.position = playerTransform.gameObject.transform.position + new Vector3(0.0f, characterDistanceAboveBall, 0.0f);
+        // direction the ball is facing
+        direction = (playerTransform.transform.position - previousPosition.normalized.normalized);
+        transform.LookAt(direction);
+        Vector3 characterRotation = new Vector3(-40.0f , 0, 0); // compensating for the wrong angle of the X
+        transform.Rotate(characterRotation);
+        // set the previous position
+        previousPosition = playerTransform.transform.position;
+    }
+    private void Start()
+    {
+        // Initial value for previous position
+        previousPosition = playerTransform.transform.position;
+    }
+}
